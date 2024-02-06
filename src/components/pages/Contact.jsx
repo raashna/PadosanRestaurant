713efefa1cea9ab365275fbb8e2de37757327ccd
Niyaz04 <niyazhaque098@gmail.com>
2024-02-07 00:57:48 +0530

    worked on Menu | Gallery | Contacts

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,17 +1,109 @@
 import React from "react";
-import './Contact.css'
+import "./Contact.css";
+import telephone from "/telephone.webp";
+import  { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_qbv0h9g", "template_jvnvb2m", form.current, {
+        publicKey: "Ji8XJnYR43KU53oNb",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          console.log("Message Sent")
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <>
-    <form>
-     <h1>Contact <span>Us</span></h1>
-     <input type ="text" name = "name" id="" placeholder="Enter name"/>
-     <input type="email" name="email" id="" placeholder="Enter Email" />
-     <input type="phone" name="phone" id="" placeholder="+91" /> 
+    
 
-     <textarea name = "message" id="" cols="30" rows="10" placeholder="Share your thoughts..."></textarea>
-     <button type="submit ">Send</button>
-    </form>
+      <div className="startIMG">
+        <img id="image" src={telephone} alt="Image of a teleponne" />
+
+        <h3 className="bgtext">Contact US</h3>
+      </div>
+
+      <div className="mainStart">
+        <div className="container">
+          <div className="content">
+            <div className="left-side">
+              <div className="address details">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="topic">Address</div>
+                <div className="text-one">The Padosan, Argora Road, Kathal More Rd</div>
+                <div className="text-two">Ranchi, Jharkhand 835303</div>
+              </div>
+              
+              <div className="phone details">
+                <i className="fas fa-phone-alt"></i>
+                <div className="topic">Phone</div>
+                <div className="text-one">9204950003</div>
+                <div className="text-two">9430196007</div>
+              </div>
+              <div className="email details">
+                <i className="fas fa-envelope"></i>
+                <div className="topic">Email</div>
+                <div className="text-one">welcomethepadosan@gmail.com</div>
+              
+              </div>
+            </div>
+            <div className="right-side">
+              <div className="topic-text">Send us a message</div>
+              <p>
+                We welcome your inquiries and feedback regarding our restaurant.
+                Please feel free to reach out to us with any queries or concerns
+                by using the form below.
+              </p>
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="input-box">
+                  <input type="text" name="user_name" placeholder="Enter your name"/>
+                </div>
+                <div className="input-box">
+                  <input type="text" name="user_email" placeholder="Enter your email"  />
+                </div>
+                <div className="input-box message-box">
+                  {/* You can add a textarea here for the message */}
+                  <textarea name="message"  placeholder="Enter your message"></textarea>
+                </div>
+                <button type="submit">
+                  <span className="button_top"  > Button</span>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Mapcontainer">
+        <div className="location-text" maptitile>
+          {" "}
+          Our Location
+        </div>
+
+        <div className="mapingthemap" style={{ width: "70%" }}>
+          <iframe
+            width="70%"
+            height="400"
+            src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=The%20Padosan,%20Argora%20Road,%20Kathal%20More%20Rd,%20Ranchi,%20Jharkhand%20835303+(The%20Padosan)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.maps.ie/population/">
+              Calculate population in area
+            </a>
+          </iframe>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,8 +1,212 @@
 import React from "react";
+import { useState } from "react";
+import "./Menu.css";
+const menuData = [
+  {
+    category: "BEVERAGES",
+    items: [
+      "mineral water",
+      "fresh lime water",
+      "soda",
+      "soda (60ml )",
+      "fresh lime soda",
+      "tea",
+      "lemon tea",
+      "green tea",
+      "masala tea",
+      "coffee",
+      "black coffee",
+      "cappuccino",
+      "cold coffee",
+      "cold coffee with ice cream",
+      "masala cold drink",
+      "cold drink (200 ml )",
+      "cold drink(300 ml )",
+      "cold drink (600 ml )",
+      "cold drink (700 ml )",
+      "cold drink ( 2 ltr.)",
+    ],
+    prices: [25,
+      36,
+      25,
+      40,
+      51,
+      26,
+      21,
+      65,
+      60,
+      42,
+      48,
+      90,
+      72,
+      100,
+      60,
+      20,
+      30,
+      50,
+      60,
+      125,],
+  },
+// Item 2
+
+
+   { category: "SNACKS",
+    items: [
+      "Peanut",
+"Peanut Masala",
+"Kaju Fry",
+"Finger Chips",
+"Veg Pakora",
+"Onion Pakora",
+"Onion Chhinta Pakora",
+"GOBHI PAKORA",
+"PANEER PAKORA",
+"CHEESE PAKORA",
+"aloo pakora",
+"egg pakora",
+"cheese balls",
+"paneer cutlet",
+"veg cutlet",
+"cheese cherry pineapple stick",
+"murg pakora"
+    ],
+    prices: [75,
+      94,
+      200,
+      150,
+      111,
+      120,
+      120,
+      120,
+      140,
+      100,
+      190,
+      140,
+      180,
+      140,
+      110,
+      170,
+      210
+      ],
+  },
+
+// Item 3
+
+
+{ category: "SANDWICH",
+items: [
+  "cheese grilled sandwich",
+  "corn spinach sandwich",
+  "paneer sandwich",
+  "veg salad sandwich",
+  "mushroom cheese sandwich",
+  "smoke grilled sandwich",
+  "masala aloo sandwich",
+  "seed american magic sandwich",
+  "veg magic sandwich"
+  
+],
+prices: [55,
+  85,
+  90,
+  66,
+  95,
+  85,
+  87,
+  72,
+  51
+  ],
+},
+
+  
+];
 
 export const Menu = () => {
-    return (
-        
-        <div>Menu</div>
-    )
-}
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [cart, setCart] = useState([]);
+
+  const handleToggleCategory = (category) => {
+    setSelectedCategory((prevCategory) =>
+      prevCategory === category ? "" : category
+    );
+  };
+
+  const handleAddToCart = (item, price) => {
+    setCart([...cart, { item, price }]);
+  };
+
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price, 0);
+  };
+
+  const handleRemoveFromCart = (index) => {
+    const updatedCart = [...cart];
+    updatedCart.splice(index, 1);
+    setCart(updatedCart);
+  };
+
+  return (
+    <div className="menu-section">
+      <div className="menu-heading">
+        <h4>Menu</h4>
+      </div>
+      <div className="menu-bar">
+        {menuData.map((categoryData) => (
+          <span
+            key={categoryData.category}
+            onClick={() => handleToggleCategory(categoryData.category)}
+            className={`menu-category ${
+              selectedCategory === categoryData.category ? "active" : ""
+            }`}
+          >
+            {categoryData.category}
+          </span>
+        ))}
+      </div>
+      <div className="menu-list">
+        {menuData.map((categoryData) => (
+          <div
+            key={categoryData.category}
+            className={`menu-category-items ${
+              selectedCategory === categoryData.category ? "visible" : ""
+            }`}
+          >
+            <h6>{categoryData.category}</h6>
+            <ul>
+              {categoryData.items.map((item, index) => (
+                <li key={index}>
+                  <span>{`${item} - Rs ${categoryData.prices[index]}`}</span>
+                  <button
+                    onClick={() =>
+                      handleAddToCart(item, categoryData.prices[index])
+                    }
+                    className="add-to-cart"
+                  >
+                    +
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="total-section">
+        <h6>Total:</h6>
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index}>
+              <span>{`${item.item} - Rs ${item.price}`}</span>
+              <button
+                onClick={() => handleRemoveFromCart(index)}
+                className="remove-from-cart"
+              >
+                -
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p>Total Price: ${calculateTotal()}</p>
+      </div>
+    </div>
+  );
+};
