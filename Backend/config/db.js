1e77f4d337eb8e72394e929825cb9748190244c9
raashna <krishnraashna@gmail.com>
2024-06-29 15:56:12 +0530

    final

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://niyazhaque098:6jTN4oi8EGTrWkF7@cluster0.rdnafwg.mongodb.net/').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.DB).then(()=>console.log("DB Connected"));
 }
